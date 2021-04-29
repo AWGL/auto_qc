1d9a1b6016b8530d1fcb503b1b14f8209a96b6a8
josephhalstead89 <josephhalstead89@gmail.com>
2021-04-29 14:09:41 +0100

    look at wren nextseq

--- deploy/auto_cron.sh
+++ deploy/auto_cron.sh
@@ -24,6 +24,8 @@ python manage.py update_database --raw_data_dir /data/archive/novaseq/BCL --conf
 echo doing the wren novaseq
 python manage.py update_database --raw_data_dir /mnt/wren_archive/novaseq/ --config /export/home/webapps/auto_qc/config/config_gen01.yaml
 
+echo doing the wren nextseq
+python manage.py update_database --raw_data_dir /mnt/wren_archive/nextseq/ --config /export/home/webapps/auto_qc/config/config_gen01.yaml
 
 
 
