6538773849c73d978c05cb98a71fec52d6d7188c
josephhalstead <josephhalstead89@gmail.com>
2022-10-25 15:28:41 +0100

    add more wes referals

--- sample_sheet/utils.py
+++ sample_sheet/utils.py
@@ -195,16 +195,36 @@ def import_worksheet_data(filepath):
                 referral_name = 'wgs~paediatric_disorders_green'
                 shire_referral_name = sample['REASON_FOR_REFERRAL']
 
-            elif referral_formatted == 'panelwesclefting':
-                referral_name = 'wes~clefting'
-                shire_referral_name = sample['REASON_FOR_REFERRAL']
-
             elif referral_formatted == 'panelwescongenanom':
                 referral_name = 'wes~paediatric_disorders_green'
                 shire_referral_name = sample['REASON_FOR_REFERRAL']
 
             elif referral_formatted == 'panelwesid':
                 referral_name = 'wes~intellectual_disability'
+                shire_referral_name = sample['REASON_FOR_REFERRAL']
+
+            elif referral_formatted == 'panelwesr101eds' or referral_formatted == 'panelwesr101':
+                referral_name = 'wes~ehlers_danlos_syndromes_green'
+                shire_referral_name = sample['REASON_FOR_REFERRAL']
+
+            elif referral_formatted == 'panelwesr125aortic' or referral_formatted == 'panelwesr125':
+                referral_name = 'wes~thoracic_aortic_aneurysm_and_dissection_green'
+                shire_referral_name = sample['REASON_FOR_REFERRAL']
+
+            elif referral_formatted == 'panelwesr15pid' or referral_formatted == 'panelwesr15':
+                referral_name = 'wes~primary_immunodeficiency_green'
+                shire_referral_name = sample['REASON_FOR_REFERRAL']
+
+            elif referral_formatted == 'panelwesr163ed' or referral_formatted == 'panelwesr163':
+                referral_name = 'wes~ectodermal_dysplasia_green'
+                shire_referral_name = sample['REASON_FOR_REFERRAL']
+
+            elif referral_formatted == 'panelwesr59epilepsy' or referral_formatted == 'panelwesr59':
+                referral_name = 'wes~genetic_epilepsy_syndromes_green'
+                shire_referral_name = sample['REASON_FOR_REFERRAL']
+
+            elif referral_formatted == 'panelwesr87cermal' or referral_formatted == 'panelwesr87':
+                referral_name = 'wes~cerebral_malformations_green'
                 shire_referral_name = sample['REASON_FOR_REFERRAL']
 
             elif referral_formatted == 'panelwesrasopathies':
@@ -215,7 +235,7 @@ def import_worksheet_data(filepath):
                 referral_name = 'wes~severe_microcephaly'
                 shire_referral_name = sample['REASON_FOR_REFERRAL']
 
-            elif referral_formatted == 'panelwesskeletalsysplasias':
+            elif referral_formatted == 'panelwesskeletaldysplasias':
                 referral_name = 'wes~skeletal_dysplasia'
                 shire_referral_name = sample['REASON_FOR_REFERRAL']
 
