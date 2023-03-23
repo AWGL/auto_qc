43bc671840aebc5a514a7ccdcf7b4a093f5b0a0d
Sophie Shaw <sophie.shaw@wales.nhs.uk>
2023-03-23 11:37:59 +0000

    Unit tests for wGS CNVs

--- qc_database/tests/test_cnv_qc.py
+++ qc_database/tests/test_cnv_qc.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 import unittest
 
-from qc_database.models import AnalysisType, CNVMetrics, Pipeline, Run, RunAnalysis, Sample, SampleAnalysis, WorkSheet
-from pipelines.parsers import parse_exome_postprocessing_cnv_qc_metrics
+from qc_database.models import AnalysisType, CNVMetrics, Pipeline, Run, RunAnalysis, Sample, SampleAnalysis, WorkSheet, DragenCNVMetrics
+from pipelines.parsers import parse_exome_postprocessing_cnv_qc_metrics, parse_dragen_cnv_metrics_file
 
 class TestParseCNVQC(unittest.TestCase):
     
@@ -150,6 +150,160 @@ class TestCNVModels(TestCase):
         CNVMetrics.objects.filter(sample_analysis=self.sample_analysis_obj).update(exome_depth_x_reference_count=1)
         passes_cnv_calling = self.sample_analysis_obj.passes_cnv_calling()
         self.assertFalse(passes_cnv_calling)
+
+class TestWGSCNVQC(unittest.TestCase):
+    
+    def setUp(self):
+
+        self.test_data_1 = "qc_database/tests/test_data/wgs_test1.cnv_metrics.csv"
+        self.test_data_2 = "qc_database/tests/test_data/wgs_test2.cnv_metrics.csv"
+
+    def test_parse_wgs_cnv_qc_metrics(self):
+        """
+        Test that WGS CNV metrics files are being parsed correctly
+        """
+        expected_1 = {
+            "bases_in_reference_genome":"3215250450",
+            "average_alignment_coverage_over_genome":"26.65",
+            "number_of_alignment_records":"620819033",
+            "number_of_filtered_records_total":"3423906",
+            "number_of_filtered_records_duplicates":"0",
+            "number_of_filtered_records_mapq":"1358349",
+            "number_of_filtered_records_unmapped":"2065557",
+            "number_of_target_intervals":"2430731",
+            "number_of_segments":"1564",
+            "number_of_amplifications":"149",
+            "number_of_deletions":"358",
+            "number_of_passing_amplifications":"87",
+            "number_of_passing_deletions":"62"
+        }
+        result_1 = parse_dragen_cnv_metrics_file(self.test_data_1)
+        self.assertEqual(expected_1, result_1)
+        
+        expected_2 = {
+            "bases_in_reference_genome":"3215250450",
+            "average_alignment_coverage_over_genome":"47.54",
+            "number_of_alignment_records":"1096341764",
+            "number_of_filtered_records_total":"6518876",
+            "number_of_filtered_records_duplicates":"0",
+            "number_of_filtered_records_mapq":"2246715",
+            "number_of_filtered_records_unmapped":"4272161",
+            "number_of_target_intervals":"2430731",
+            "number_of_segments":"1700",
+            "number_of_amplifications":"140",
+            "number_of_deletions":"530",
+            "number_of_passing_amplifications":"79",
+            "number_of_passing_deletions":"62"
+        }
+        result_2 = parse_dragen_cnv_metrics_file(self.test_data_2)
+        self.assertEqual(expected_2, result_2)
+
+class test_WGS_cnv_checks(unittest.TestCase):  
+
+    def setUp(self):
+ 
+        Run.objects.create(
+            run_id = "TEST_RUN"
+        )
+        self.run_obj = Run.objects.get(run_id="TEST_RUN")
+
+        Sample.objects.create(
+            sample_id = "sample_1"
+        )
+        self.sample1_obj = Sample.objects.get(sample_id="sample_1")
+        
+        Sample.objects.create(
+            sample_id = "sample_2"
+        )
+        self.sample2_obj = Sample.objects.get(sample_id="sample_2")
+
+        Pipeline.objects.create(
+            pipeline_id = "DragenWGS-master"
+        )
+        self.pipeline_obj = Pipeline.objects.get(pipeline_id="DragenWGS-master")
+
+        AnalysisType.objects.create(
+            analysis_type_id = "WGS"
+        )
+        self.analysis_type_obj = AnalysisType.objects.get(analysis_type_id="WGS")
+
+        WorkSheet.objects.create(
+            worksheet_id = "TEST_WORKSHEET"
+        )
+        self.worksheet_obj = WorkSheet.objects.get(worksheet_id="TEST_WORKSHEET")
+
+        RunAnalysis.objects.create(
+            run=self.run_obj,
+            pipeline=self.pipeline_obj,
+            analysis_type=self.analysis_type_obj,
+            max_cnv_calls=300,
+            min_average_coverage_cutoff=30
+        )
+
+        SampleAnalysis.objects.create(
+            sample=self.sample1_obj,
+            run=self.run_obj,
+            pipeline=self.pipeline_obj,
+            analysis_type=self.analysis_type_obj,
+            worksheet=self.worksheet_obj,
+            results_completed=True,
+            results_valid=True,
+            sex="male",
+            contamination_cutoff=10,
+            ntc_contamination_cutoff=10,
+            max_cnvs_called_cutoff=300,
+            min_average_coverage_cutoff=30
+        )
+        self.sample_analysis_obj_1 = SampleAnalysis.objects.get(sample=self.sample1_obj)
+        
+        SampleAnalysis.objects.create(
+            sample=self.sample2_obj,
+            run=self.run_obj,
+            pipeline=self.pipeline_obj,
+            analysis_type=self.analysis_type_obj,
+            worksheet=self.worksheet_obj,
+            results_completed=True,
+            results_valid=True,
+            sex="male",
+            contamination_cutoff=10,
+            ntc_contamination_cutoff=10,
+            max_cnvs_called_cutoff=300,
+            min_average_coverage_cutoff=30
+        )
+        self.sample_analysis_obj_2 = SampleAnalysis.objects.get(sample=self.sample2_obj)  
+        
+        DragenCNVMetrics.objects.create(
+            sample_analysis = self.sample_analysis_obj_1,
+            average_alignment_coverage_over_genome = 26.65,
+            number_of_passing_amplifications = 87,
+            number_of_passing_deletions = 62
+        )
+        self.dragen_cnv_metrics_1 = DragenCNVMetrics.objects.get(sample_analysis=self.sample_analysis_obj_1)
+        
+        DragenCNVMetrics.objects.create(
+            sample_analysis = self.sample_analysis_obj_2,
+            average_alignment_coverage_over_genome = 47.54,
+            number_of_passing_amplifications = 200,
+            number_of_passing_deletions = 200
+        )
+        self.dragen_cnv_metrics_2 = DragenCNVMetrics.objects.get(sample_analysis=self.sample_analysis_obj_2)
+    
+    def test_cnv_checks(self):
+        """
+        Test that CNV metrics populate models correctly and tests pass/fail as expected
+        """
+        passes_cnv_coverage_1 = self.sample_analysis_obj_1.passes_average_coverage()
+        self.assertFalse(passes_cnv_coverage_1)
+
+        passes_cnv_coverage_2 = self.sample_analysis_obj_2.passes_average_coverage()
+        self.assertTrue(passes_cnv_coverage_2)
+        
+        passes_cnv_count_1 = self.sample_analysis_obj_1.passes_cnv_count()
+        self.assertTrue(passes_cnv_count_1)
+        
+        passes_cnv_count_2 = self.sample_analysis_obj_2.passes_cnv_count()
+        self.assertFalse(passes_cnv_count_2)
+        
 
 if __name__ == "__main__":
     unittest.main()
