77f5a91c87b67ebd266fbce6f612187ef1ccee5a
Istaisa <32853927+Istaisa@users.noreply.github.com>
2023-08-08 11:29:46 +0100

    edited fix to account for both assays

--- deploy/fix_sample_sheets.sh
+++ deploy/fix_sample_sheets.sh
@@ -1,6 +1,4 @@
-cat SampleSheet.csv | sed 's/Name$ge/pipelineName=germline_enrichment_nextflow;pipelineVersion=master/' | sed 's/%/;/g' | sed 's/\$/=/g' > SampleSheet_fixed.csv
+cat SampleSheet.csv | sed 's/Name$ge/pipelineName=germline_enrichment_nextflow;pipelineVersion=master/' | sed 's/Name$SA/pipelineName=SomaticAmplicon;pipelineVersion=master/' | sed 's/panel=NGHS101X/panel=NGHS-101X/' | sed 's/NGHS102X/NGHS-102X/' | sed 's/ref=/referral=/' | sed 's/%/;/g' | sed 's/\$/=/g' > SampleSheet_fixed.csv
 
-cat SampleSheet.csv | sed 's/Name$SA/pipelineName=SomaticAmplicon;pipelineVersion=master/' | sed 's/%/;/g' | sed 's/\$/=/g' | sed 's/panel=NGHS101X/panel=NGHS-101X/' | sed 's/NGHS102X/NGHS-102X/' | sed 's/ref=/referral=/' > SampleSheet_fixed2.csv
-
-mv SampleSheet_fixed2.csv SampleSheet.csv
+mv SampleSheet_fixed.csv SampleSheet.csv
 
