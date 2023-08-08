c5ff0e7607f942794988569e01c10bf0fe760db5
Istaisa <32853927+Istaisa@users.noreply.github.com>
2023-08-08 11:49:34 +0100

    fixed fixing command

--- deploy/fix_sample_sheets.sh
+++ deploy/fix_sample_sheets.sh
@@ -1,4 +1,4 @@
-cat SampleSheet.csv | sed 's/Name$ge/pipelineName=germline_enrichment_nextflow;pipelineVersion=master/' | sed 's/Name$SA/pipelineName=SomaticAmplicon;pipelineVersion=master/' | sed 's/panel=NGHS101X/panel=NGHS-101X/' | sed 's/NGHS102X/NGHS-102X/' | sed 's/ref=/referral=/' | sed 's/%/;/g' | sed 's/\$/=/g' > SampleSheet_fixed.csv
+cat SampleSheet.csv | sed 's/Name$ge/pipelineName=germline_enrichment_nextflow;pipelineVersion=master/' | sed 's/Name$SA/pipelineName=SomaticAmplicon;pipelineVersion=master/' | sed 's/NGHS101X/NGHS-101X/' | sed 's/NGHS102X/NGHS-102X/' | sed 's/ref/referral/' | sed 's/%/;/g' | sed 's/\$/=/g' > SampleSheet_fixed.csv
 
 mv SampleSheet_fixed.csv SampleSheet.csv
 
