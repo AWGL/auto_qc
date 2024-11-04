927dc576774921c694dd64474bb76fb33124aa50
webserver_autoqc <bioinformatics.team@wales.nhs.uk>
2024-11-04 12:33:40 +0000

    Add timestamps to cron log

--- deploy/auto_cron.sh
+++ deploy/auto_cron.sh
@@ -6,13 +6,13 @@ source /home/autoqc/miniconda3/bin/activate auto_qc
 cd /u01/apps/autoqc/auto_qc/
 
 
-echo doing the wren novaseq
+echo -e INFO"\t"$(date +%Y-%m-%d_%H-%M-%S)"\t"doing the wren novaseq
 python manage.py update_database --raw_data_dir /mnt/wren/wren_archive/quality_temp/novaseq/ --config /u01/apps/autoqc/auto_qc/config/config_webserver.yaml
 
-echo doing the wren miseq
+echo -e INFO"\t"$(date +%Y-%m-%d_%H-%M-%S)"\t"doing the wren miseq
 python manage.py update_database --raw_data_dir /mnt/wren/wren_archive/quality_temp/miseq/ --config /u01/apps/autoqc/auto_qc/config/config_webserver.yaml
 
-echo doing the wren nextseq
+echo -e INFO"\t"$(date +%Y-%m-%d_%H-%M-%S)"\t"doing the wren nextseq
 python manage.py update_database --raw_data_dir /mnt/wren/wren_archive/quality_temp/nextseq/ --config /u01/apps/autoqc/auto_qc/config/config_webserver.yaml
 
 source /home/autoqc/miniconda3/bin/deactivate
