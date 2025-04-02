9c1f280e22a384eaea1af3af1ea5c4b64a27f7db
josephhalstead <josephhalstead89@gmail.com>
2025-04-02 11:21:29 +0100

    local

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-deploy_location = 'gen01'
+deploy_location = 'local'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
