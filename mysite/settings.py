68c341bb19088fefaa6348d755891d7e29d433ee
SophieS9 <sophie.shaw@wales.nhs.uk>
2025-07-28 13:44:57 +0100

    Removing unused message tags in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from django.contrib import messages
 
-
 deploy_location = 'webserver'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -80,13 +79,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
-MESSAGE_TAGS = {
-    messages.DEBUG: 'secondary',
-    messages.INFO: 'info',
-    messages.SUCCESS: 'success',
-    messages.WARNING: 'warning',
-    messages.ERROR: 'danger',
-}
 
 # Database
 
